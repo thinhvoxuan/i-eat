8fc7db9f918dd9078abb2f8b73e9d03e586d8691
thinhvoxuan <thinhvoxuan@gmail.com>
2018-08-27 09:00:09 +0700

    init project with slack command

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/shomali11/slacker"
+)
+
+func main() {
+	godotenv.Load()
+	bot := slacker.NewClient(os.Getenv("TOKEN"))
+
+	bot.Command("Menu", "List of good today", func(request slacker.Request, response slacker.ResponseWriter) {
+		response.Reply("List of good today")
+	})
+
+	bot.Command("Add <number>", "Add food", func(request slacker.Request, response slacker.ResponseWriter) {
+		number := request.IntegerParam("number", 1)
+		response.Reply(fmt.Sprintf("Add %d", number))
+	})
+
+	bot.Command("Remove <number>", "Remove food", func(request slacker.Request, response slacker.ResponseWriter) {
+		number := request.IntegerParam("number", 1)
+		response.Reply(fmt.Sprintf("Remove %d", number))
+	})
+
+	bot.Command("Reset", "Remove all foods", func(request slacker.Request, response slacker.ResponseWriter) {
+		response.Reply("Remove all foods")
+	})
+
+	bot.Command("Review", "Review your food", func(request slacker.Request, response slacker.ResponseWriter) {
+		response.Reply("Review your food")
+	})
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err := bot.Listen(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
